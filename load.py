2bdd34f4ecd3e5b0d93abf6a3c28d8211a408843
Michael Ferguson <mfergs7@gmail.com>
2023-04-26 13:54:03 -0700

    not quite working

---
+++ load.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Copyright 2023 Michael Ferguson
+# All Rights Reserved
+
+# https://github.com/LiamBindle/PyVESC
+from pyvesc import VESC
+from math import pi
+
+class LoadInterface:
+
+    # Motor has 14 pole pairs - 7 poles
+    motor_poles = 7
+
+    # Torque constant
+    # Kt = 60 / (2 * pi * Kv) = 60 / (2 * pi * 149)
+    kt = 0.064
+
+    def __init__(self, port="/dev/ttyACM0"):
+        self.vesc = VESC(serial_port=port, start_heartbeat=False)
+
+    def set_duty_cycle(self, duty_cycle):
+        self.vesc.set_duty_cycle(duty_cycle)
+
+    def set_radians_per_sec(self, rads_sec):
+        rpm = int(60.0 * rads_sec / (2 * pi) * self.motor_poles)
+        print(rads_sec, rpm)
+        self.vesc.set_rpm(rpm)
+
+    def set_torque(self, torque):
+        milliamps = int((torque / self.kt) * 1000.0)
+        print(torque, milliamps)
+        self.vesc.set_current(milliamps)
+
+
+# this is just testing stuff
+if __name__ == "__main__":
+    import time
+    l = LoadInterface()
+    while True:
+        #l.set_duty_cycle(0.02)
+        l.set_radians_per_sec(25.0)
+        time.sleep(0.01)
